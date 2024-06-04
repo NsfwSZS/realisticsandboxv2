7bf4d595519f599761e1b2661280ed765ce0432d
Nsfw <nik.grikorev@gmail.com>
2024-06-05 00:08:53 +0400

    new file:   entities/weapons/nsbase_handcuffs/cl_init.lua 	deleted:    entities/weapons/weapon_beretta.lua 	modified:   gamemode/modules/sv_workshop.lua

--- gamemode/modules/sv_workshop.lua
+++ gamemode/modules/sv_workshop.lua
@@ -2,7 +2,7 @@
 local workshopid = {
     "3256245130", -- https://steamcommunity.com/sharedfiles/filedetails/?id=3256245130 models 1
     "3258336192", -- https://steamcommunity.com/sharedfiles/filedetails/?id=3258336192 models 2
-    -- "3141596838", -- https://steamcommunity.com/sharedfiles/filedetails/?id=3141596838 map
+    "3141596838", -- https://steamcommunity.com/sharedfiles/filedetails/?id=3141596838 map
     "3258989742", -- https://steamcommunity.com/sharedfiles/filedetails/?id=3258989742 jmod models
 }
 
